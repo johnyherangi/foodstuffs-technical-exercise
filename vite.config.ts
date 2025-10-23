84d24113ac837735094d7b2ee6e30eb4d4be4298
Johny Herangi <johnyherangi@gmail.com>
2025-10-24 02:08:05 +1300

    exclude e2e tests from vitest run

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
+    exclude: ["e2e/*"],
     globals: true,
     mockReset: true,
     restoreMocks: true,
