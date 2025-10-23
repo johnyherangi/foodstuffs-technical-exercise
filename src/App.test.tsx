75d8754ad2a6eae5f2574385a2d27e20799b8016
Johny Herangi <johnyherangi@gmail.com>
2025-10-24 00:56:42 +1300

    add unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import App from "./App"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
 
 describe("App", () => {
@@ -7,5 +7,22 @@ describe("App", () => {
     const { asFragment } = render(<App />)
 
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("has accessible elements", () => {
+    render(<App />)
+
+    expect(
+      screen.getByRole("spinbutton", { name: "Number of items" })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("spinbutton", { name: "Price per item ($)" })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("combobox", { name: "State code" })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("button", { name: "Calculate total" })
+    ).toBeInTheDocument()
   })
 })
